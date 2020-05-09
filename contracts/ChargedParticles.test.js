a094b158c6fb6841e35ab1bdcfeab448568c7da6
Keith Yeung <kungfukeith11@gmail.com>
2020-05-08 19:26:25 -0700

    Add unit tests for operations with the ion token

--- contracts/ChargedParticles.test.js
+++ contracts/ChargedParticles.test.js
@@ -10,7 +10,7 @@ const ChargedParticles = Contracts.getFromLocal('ChargedParticles');
 const ChargedParticlesERC1155 = Contracts.getFromLocal('ChargedParticlesERC1155');
 
 describe('ChargedParticles', () => {
-  let [owner, nonOwner] = accounts;
+  let [owner, nonOwner, ionHodler] = accounts;
   let contractInstance;
   let tokenManagerInstance;
   let helper;
@@ -145,6 +145,7 @@ describe('ChargedParticles', () => {
         const bridgeAddress = await contractInstance.methods.getTypeTokenBridge(ionTokenId).call({ from: nonOwner });
         expect(bridgeAddress).not.toBe(owner);
         expect(bridgeAddress).not.toBe(nonOwner);
+        expect(bridgeAddress).not.toBe(ionHodler);
         expect(bridgeAddress).not.toBe(contractInstance.address);
         expect(bridgeAddress).not.toBe(tokenManagerInstance.address);
       });
@@ -174,6 +175,145 @@ describe('ChargedParticles', () => {
       test('getTotalMinted', async () => {
         const totalMinted = await contractInstance.methods.getTotalMinted(ionTokenId).call({ from: nonOwner });
         expect(totalMinted).toBe('42');
+      });
+    });
+
+    describe('Operations on Ion token', () => {
+      let ionTokenId;
+
+      beforeEach(async () => {
+        await contractInstance.methods.registerTokenManager(tokenManagerInstance.address).send({ from: owner });
+        await tokenManagerInstance.methods.setChargedParticles(contractInstance.address).send({ from: owner });
+        const receipt = await contractInstance.methods.mintIons(
+          'https://www.example.com',
+          1337,
+          42,
+          web3.utils.toWei('1', 'ether')
+        ).send({ from: owner, gas: 5e6 });
+        ionTokenId = receipt.events['PlasmaTypeUpdated'].returnValues['_plasmaTypeId'];
+      });
+
+      test('mintParticle', async () => {
+        await contractInstance.methods.setPausedState(true).send({ from: owner });
+        await expectRevert(
+          contractInstance.methods.mintParticle(ionHodler, ionTokenId, 10, '', []).send({ from: owner }),
+          "E417"
+        );
+        await contractInstance.methods.setPausedState(false).send({ from: owner });
+
+        await expectRevert(
+          contractInstance.methods.mintParticle(ionHodler, ionTokenId, 10, '', []).send({ from: owner }),
+          "E104"
+        );
+      });
+
+      test('burnParticle', async () => {
+        expectRevert(
+          contractInstance.methods.burnParticle(ionTokenId).send({ from: owner }),
+          "E104"
+        );
+      });
+
+      test('energizeParticle', async () => {
+        expectRevert(
+          contractInstance.methods.energizeParticle(ionTokenId, 10).send({ from: owner }),
+          "E104"
+        );
+      });
+
+      test('mintPlasma from creator', async () => {
+        await contractInstance.methods.setPausedState(true).send({ from: owner });
+        await expectRevert(
+          contractInstance.methods.mintPlasma(ionHodler, ionTokenId, 10, []).send({ from: owner }),
+          "E417"
+        );
+        await contractInstance.methods.setPausedState(false).send({ from: owner });
+
+        // Mint amount exceends limit
+        await expectRevert(
+          contractInstance.methods.mintPlasma(ionHodler, ionTokenId, 9999, []).send({ from: owner }),
+          "E407"
+        );
+
+        const receipt = await contractInstance.methods.mintPlasma(ionHodler, ionTokenId, 3, []).send({ from: owner, gas: 5e6 });
+        expectEvent(receipt, 'PlasmaMinted', {
+          _sender: owner,
+          _receiver: ionHodler,
+          _typeId: ionTokenId,
+          _amount: '3'
+        });
+        expect(await tokenManagerInstance.methods.balanceOf(ionHodler, ionTokenId).call()).toBe('3');
+      });
+
+      test('mintPlasma from non-creator', async () => {
+        // Mint amount exceends limit
+        await expectRevert(
+          contractInstance.methods.mintPlasma(ionHodler, ionTokenId, 9999, []).send({ from: nonOwner }),
+          "E407"
+        );
+
+        // Not including the minting fee with the transaction
+        await expectRevert(
+          contractInstance.methods.mintPlasma(ionHodler, ionTokenId, 10, []).send({ from: nonOwner, gas: 5e6 }),
+          "E404"
+        );
+
+        const receipt = await contractInstance.methods
+          .mintPlasma(ionHodler, ionTokenId, 3, [])
+          .send({ from: nonOwner, gas: 5e6, value: web3.utils.toWei('5', 'ether') });
+
+        expectEvent(receipt, 'PlasmaMinted', {
+          _sender: nonOwner,
+          _receiver: ionHodler,
+          _typeId: ionTokenId,
+          _amount: '3'
+        });
+
+        const balance = await tokenManagerInstance.methods.balanceOf(ionHodler, ionTokenId).call({ from: ionHodler });
+        expect(balance).toBe('3');
+      });
+
+      test('withdrawFees', async () => {
+        await contractInstance.methods.mintPlasma(ionHodler, ionTokenId, 3, []).send({ from: nonOwner, gas: 5e6, value: web3.utils.toWei('5', 'ether') });
+
+        expectRevert(
+          contractInstance.methods.withdrawFees(ionHodler).send({ from: nonOwner, gas: 5e6 }),
+          "Ownable: caller is not the owner"
+        );
+
+        expectRevert(
+          contractInstance.methods.withdrawFees('0x0000000000000000000000000000000000000000').send({ from: owner, gas: 5e6 }),
+          "E412"
+        );
+
+        const balanceBefore = await web3.eth.getBalance(ionHodler);
+        const receipt = await contractInstance.methods.withdrawFees(ionHodler).send({ from: owner, gas: 5e6 });
+        const balanceAfter = await web3.eth.getBalance(ionHodler);
+
+        expectEvent(receipt, 'ContractFeesWithdrawn', {
+          _sender: owner,
+          _receiver: ionHodler,
+          _amount: web3.utils.toWei('3', 'ether').toString()
+        });
+        expect(web3.utils.fromWei((balanceAfter - balanceBefore).toString(), 'ether')).toBe('3');
+      });
+
+      test('burnPlasma', async () => {
+        expectRevert(
+          contractInstance.methods.burnPlasma(ionTokenId, 10).send({ from: nonOwner }),
+          "E106"
+        );
+
+        const receipt = await contractInstance.methods.burnPlasma(ionTokenId, 10).send({ from: owner });
+  
+        expectEvent(receipt, 'PlasmaBurned', {
+          _from: owner,
+          _typeId: ionTokenId,
+          _amount: '10'
+        });
+
+        const balance = await tokenManagerInstance.methods.balanceOf(owner, ionTokenId).call({ from: owner });
+        expect(balance).toBe('32');
       });
     });
   });
